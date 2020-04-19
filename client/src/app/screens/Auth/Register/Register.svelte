d0eaf30c79b9e15b3a2a5d3125a2323ae7b5ada5
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-04-20 01:19:24 +0530

    get and display departmenst in faculty

--- client/src/app/screens/Auth/Register/Register.svelte
+++ client/src/app/screens/Auth/Register/Register.svelte
@@ -12,6 +12,7 @@
     {
       faculties {
         name
+        _id
       }
     }
   `;
@@ -19,13 +20,36 @@
   const getCliet = getClient();
   const faculties = query(getCliet, { query: getFaculties });
 
+  const getDepartments = gql`
+    mutation($facultyId: String!) {
+      departments(facultyId: $facultyId) {
+        name
+      }
+    }
+  `;
+
   let username = "";
   let email = "";
   let password = "";
   let confirmPassword;
   let faculty;
+  let departments = [];
+  let department;
 
-  $: console.log(faculty);
+  $: (() => {
+    mutate(client, {
+      mutation: getDepartments,
+      variables: { facultyId: faculty }
+    })
+      .then(res => {
+        if (res && res.data && res.data.departments) {
+          departments = res.data.departments;
+        }
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
+  })();
 
   $: (() => {
     if (password !== confirmPassword) {
@@ -49,11 +73,21 @@
 <UnAuthProtect>
   <div slot="content" class="container login_form">
     <form>
+      <div class="form-group">
+        <label>Username</label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Username"
+          bind:value={username} />
+        {#if (username && username.length < 4) || username.length > 12}
+          <div class="alert alert-danger">
+            username should be 4 to 12 characters
+          </div>
+        {/if}
+
+      </div>
       <div class="form-row">
-        <div class="form-group col-md-6">
-          <label>Username</label>
-          <input type="text" class="form-control" placeholder="Username" />
-        </div>
         <div class="form-group col-md-6">
           <label>Faculty</label>
 
@@ -66,13 +100,28 @@
               Loading....
             {:then result}
               {#each result.data.faculties as faculty}
-                <option value={faculty.name}>{faculty.name}</option>
+                <option value={faculty._id}>{faculty.name}</option>
               {/each}
             {:catch error}
               {error.message}
             {/await}
           </select>
         </div>
+        <div class="form-group col-md-6">
+          <label>Department</label>
+
+          <select
+            class="custom-select mr-sm-2"
+            id="inlineFormCustomSelect"
+            bind:value={faculty}>
+
+            {#each departments as department}
+              <option value={department.name}>{department.name}</option>
+            {/each}
+
+          </select>
+        </div>
+
       </div>
 
       <div class="form-group">
