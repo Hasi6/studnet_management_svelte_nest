e50fcac7df4dad43efb705e2e922b51539ca58d9
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-04-24 21:51:01 +0530

    add redux setup

--- client_react/src/app/App.tsx
+++ client_react/src/app/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Provider store={store}>
+      <div>Hasi</div>
+    </Provider>
+  );
 };
 
 export default App;
--- client_react/src/app/redux/reducers/util/reducerUtil.ts
+++ client_react/src/app/redux/reducers/util/reducerUtil.ts
@@ -1,5 +1,5 @@
-export const createReducer = (initialState, fnMap) => {
-  return (state = initialState, { type, payload }) => {
+export const createReducer = (initialState: any, fnMap: any) => {
+  return (state = initialState, { type, payload }: { type: any, payload: any }) => {
     const handler = fnMap[type];
     return handler ? handler(state, payload) : state;
   };
