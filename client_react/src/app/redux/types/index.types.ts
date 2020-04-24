8f7561129c74d934dfeab4d331055a5daf2daa83
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-04-24 21:13:25 +0530

    make some changes

---
+++ client_react/src/app/redux/types/index.types.ts
@@ -0,0 +1,7 @@
+// Auth Types
+export enum AuthTypes {
+    CURRENT_USER = "CURRENT_USER",
+    REGISTER_USER = "REGISTER_USER",
+    LOGIN_USER = "LOGIN_USER",
+    LOG_OUT_USER = "LOG_OUT_USER",
+};
