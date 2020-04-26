b08a54058d7bc6abe03d1d3c7aa26377e84b7824
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-04-26 22:17:58 +0530

    make change

--- client/src/components/HomePage/Messages/MessagesComponent/MessageCard/SingleMessage/SingleMessage.svelte
+++ client/src/components/HomePage/Messages/MessagesComponent/MessageCard/SingleMessage/SingleMessage.svelte
@@ -1,20 +1,36 @@
 <script>
-  import { Card } from "svelte-chota";
+  import { Card, Button, Tag } from "svelte-chota";
+  import { mdiChevronDown } from "@mdi/js";
   export let isMe;
 </script>
 
 <style>
-
+  .message {
+    display: flex;
+  }
 </style>
 
 <Card
   style={isMe ? 'max-width: 400px; background:darkgreen;  margin-bottom: 15px; position:relative; left:200px' : 'max-width: 400px; margin-bottom: 15px'}>
-  <p style="width:auto">
-    Hey Tiro Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
-    sapiente suscipit natus sed illum, corporis facilis porro eos dolores neque
-    obcaecati voluptatibus itaque nemo voluptates nostrum cupiditate ex at
-    blanditiis.
-  </p>
+
+  <div class="message">
+
+    <p style="width:auto">
+      Hey Tiro Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+      sapiente suscipit natus sed illum, corporis facilis porro eos dolores
+      neque obcaecati voluptatibus itaque nemo voluptates nostrum cupiditate ex
+      at blanditiis.
+    </p>
+    <Button
+      dropdown=""
+      autoclose
+      outline
+      iconRight={mdiChevronDown}
+      style="float:right; border:none">
+      <p>Delete from Me</p>
+      <p>Delete From All</p>
+    </Button>
+  </div>
   <div>
     <p style="width:auto">Time</p>
     {#if isMe}
