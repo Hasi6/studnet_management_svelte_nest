b2c406eb898a395d3ce1c049b1169274e4158e86
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-05-01 22:04:34 +0530

    create proper auth guard for graphql queries

--- server_chat/src/user/AuthGuard.ts
+++ server_chat/src/user/AuthGuard.ts
@@ -1,11 +1,46 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import * as jwt from 'jsonwebtoken'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean {
+
+    constructor(
+        private readonly userService: UserService
+    ) {
+    }
+    // jwtSecret
+    public validate = async (token: string) => {
+        try {
+            const decode: any = await jwt.verify(token, "jwtSecret")
+
+            const { _id } = decode;
+
+            const user = await this.userService.getUserById(_id);
+            return user
+        } catch (err) {
+            throw new UnauthorizedException()
+        }
+    }
+
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
         const ctx: any = GqlExecutionContext.create(context).getContext();
-        console.log(ctx.headers.authorization)
-        return true;
+
+        if (!ctx?.headers?.authorization) {
+            throw new UnauthorizedException()
+        }
+
+        const token = ctx.headers.authorization.split(" ")
+
+        if (token?.length > 0 && token[0] !== "Bearer") {
+            throw new UnauthorizedException("")
+        }
+
+        const user = await this.validate(token[1])
+        ctx.user = user;
+        return true
+
     }
 }
