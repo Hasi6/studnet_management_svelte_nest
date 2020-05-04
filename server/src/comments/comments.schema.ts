3c48af9ef4bb92203c89b764954a5208d5f48f4a
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-05-04 11:35:56 +0530

    create comments schema

---
+++ server/src/comments/comments.schema.ts
@@ -0,0 +1,23 @@
+import { Schema } from "mongoose";
+
+
+
+export const CommentsSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "users",
+            required: true,
+        },
+        post: {
+            type: Schema.Types.ObjectId,
+            ref: "posts",
+            required: true,
+        },
+        message: {
+            type: String,
+            required: true,
+        }
+    },
+    { timestamps: true }
+);
