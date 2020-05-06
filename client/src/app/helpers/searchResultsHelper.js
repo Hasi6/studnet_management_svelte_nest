4e3d2c7a028cc758831a9392f20ec4ccee8b7d04
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-05-06 23:20:20 +0530

    create search result helper function

---
+++ client/src/app/helpers/searchResultsHelper.js
@@ -0,0 +1,25 @@
+// Search Result Helper
+// https://levelup.gitconnected.com/use-regex-and-javascript-to-improve-search-results-870932050d08
+const checkName = (name, str) => {
+  var pattern = str
+    .split("")
+    .map(x => {
+      return `(?=.*${x})`;
+    })
+    .join("");
+  var regex = new RegExp(`${pattern}`, "g");
+  return name.match(regex);
+};
+
+// Search Messages
+export const searchMessages = (e, messages, allMessages) => {
+  if (!e) {
+    return allMessages;
+  }
+  const str = e.toLowerCase().substring(0, 3);
+  const searchMessages = messages.filter(msg => {
+    const xSub = msg.message.substring(0, 3).toLowerCase();
+    return msg.message.toLowerCase().includes(str) || checkName(xSub, str);
+  });
+  return searchMessages;
+};
