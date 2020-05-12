c99b3ce241b23d6ed3a0762c74ab908250411225
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-05-12 18:28:37 +0530

    add ui

--- client_react/src/app/screens/EventDetails/EventDetails.tsx
+++ client_react/src/app/screens/EventDetails/EventDetails.tsx
@@ -2,8 +2,24 @@
 import React, { FC, useEffect } from "react";
 import { connect } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
 import { IEvents } from "../../model/User.model";
 import { getSingleEvent } from "../../redux/actions/events/events.actions";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 1000
+  }
+});
 
 interface propTypes {
   event: IEvents[];
@@ -14,6 +30,8 @@ const EventDetails: FC<propTypes> = ({
   event,
   getSingleEvent
 }): JSX.Element => {
+  const classes = useStyles();
+
   const { id } = useParams();
   const history = useHistory();
 
@@ -27,7 +45,38 @@ const EventDetails: FC<propTypes> = ({
     getEvent();
   }, [event]);
 
-  return <div>{JSON.stringify(id)}</div>;
+  console.log(event);
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image={event[0]?.image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 const mapStateToProps = (state: any, ownProps: any) => {
