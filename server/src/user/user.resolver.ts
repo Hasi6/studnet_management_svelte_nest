d241e54cda1321bba86a3ce4ea8f4143e794c553
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-05-17 22:03:11 +0530

    mutation

--- server/src/user/user.resolver.ts
+++ server/src/user/user.resolver.ts
@@ -1,4 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { UserService } from './user.service';
 
 @Resolver('User')
-export class UserResolver {}
+export class UserResolver {
+
+    constructor(
+        private readonly userService: UserService
+    ) { }
+
+
+    @Mutation()
+    register(@Args('input') input: any) {
+        return this.userService.register(input);
+    }
+
+    @Mutation()
+    login(@Args('input') input: any) {
+        return this.userService.login(input);
+    }
+
+}
