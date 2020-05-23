c5e61b6a047eafedcbe29e94aa009c1b9e1f3d9b
Hasi6 <hasitha.chandula@gmail.com>
2020-05-23 07:56:14 +0530

    as

--- client_react/src/index.tsx
+++ client_react/src/index.tsx
@@ -1,31 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "./bootstrap.css";
-import App from "./app/App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import './bootstrap.css'
+import App from './app/App'
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom'
 
 // React Prime
-import "primereact/resources/themes/nova-light/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
+import 'primereact/resources/themes/nova-light/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
 
-const rootEl = document.getElementById("root");
-declare const module: any;
+const rootEl = document.getElementById('root')
+declare const module: any
+
+// hey
 
 let render = () =>
   ReactDOM.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>,
-    rootEl
-  );
+    rootEl,
+  )
 
 if (module.hot) {
   module.hot.accept(App, () => {
-    setTimeout(render);
-  });
+    setTimeout(render)
+  })
 }
 
-render();
+render()
