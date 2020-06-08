c8d73445b4ed4523a78120bfaabed701191af167
Hasi6 <hasitha.chandula@gmail.com>
2020-06-08 18:07:30 +0530

    make changes

--- server_chat/src/app.module.ts
+++ server_chat/src/app.module.ts
@@ -1,30 +1,34 @@
-import { GraphQLModule } from '@nestjs/graphql';
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose'
+import { GraphQLModule } from "@nestjs/graphql";
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 
-
-import { FacultiesModule } from './faculties/faculties.module';
-import { UserModule } from './user/user.module';
-import { DepartmentModule } from './department/department.module';
-import { CoursesModule } from './courses/courses.module';
-import { ProfileModule } from './profile/profile.module';
-import { ChatModule } from './chat/chat.module';
-import { MessagesModule } from './messages/messages.module';
+import { FacultiesModule } from "./faculties/faculties.module";
+import { UserModule } from "./user/user.module";
+import { DepartmentModule } from "./department/department.module";
+import { CoursesModule } from "./courses/courses.module";
+import { ProfileModule } from "./profile/profile.module";
+import { ChatModule } from "./chat/chat.module";
+import { MessagesModule } from "./messages/messages.module";
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.NODE_ENV === 'production' ? "mongodb://localhost:27017/studentManagement-svelte-nest" : "mongodb://localhost:27017/studentManagement-svelte-nest", {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    }),
+    MongooseModule.forRoot(
+      process.env.NODE_ENV === "production"
+        ? "mongodb://localhost:27017/studentManagement-svelte-nest"
+        : "mongodb://localhost:27017/studentManagement-svelte-nest",
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      },
+    ),
     GraphQLModule.forRoot({
       debug: true,
       playground: true,
       installSubscriptionHandlers: true,
       context: ({ req }) => ({ headers: req.headers }),
-      typePaths: ['./**/*.graphql']
+      typePaths: ["./**/*.graphql"],
     }),
     FacultiesModule,
     UserModule,
@@ -32,9 +36,9 @@ import { MessagesModule } from './messages/messages.module';
     CoursesModule,
     ProfileModule,
     ChatModule,
-    MessagesModule
+    MessagesModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
